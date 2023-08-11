fc7926709f22a57dac1e40b4aabacaa205bf7e19
azriel-stephen <azrielstephen@gmail.com>
2023-08-12 03:27:47 +0530

    laid foundation

--- app.js
+++ app.js
@@ -1,14 +1,38 @@
 import ReactDOM from "react-dom/client";
-const jsxHeading = (
-  <h1 className="head" tabIndex="5">
-    Hello world from JSX
-  </h1>
+import { createElement } from "react";
+
+const nestedHeader = createElement("div", { className: "title" }, [
+  createElement("h1", {}, "Nested Header h1"),
+  createElement("h2", {}, "Nested Header h2"),
+  createElement("h3", {}, "Nested Header h3"),
+]);
+
+const nestedJSX = (
+  <div className="title">
+    <h1>Nested Header h1</h1>
+    <h2>Nested Header h2</h2>
+    <h3>Nested Header h3</h3>
+  </div>
 );
 
-// JSX => Babel transpiles the code to React.createElement => ReactElement-JS object => HTML element(render).
+const Heading = () => <h1>Heading of something</h1>;
 
-console.log(jsxHeading);
+const NestedComponent = () => {
+  return (
+    <div className="title">
+      <Heading />
+      {Heading()}
+      <Heading></Heading>
+      <h1>Nested Header h1</h1>
+      <h2>Nested Header h2</h2>
+      <h3>Nested Header h3</h3>
+    </div>
+  );
+};
+console.log(nestedHeader, typeof nestedHeader);
+console.log(nestedJSX, typeof nestedJSX);
+console.log(NestedComponent, typeof NestedComponent);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);
+root.render(<NestedComponent />);
+// root.render(nestedJSX);
